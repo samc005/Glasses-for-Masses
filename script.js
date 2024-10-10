7d6684b6cace5581994da7b5c6cf3691d6d6a8d9
SC Chak <15221314-SChak@users.noreply.replit.com>
2024-10-10 17:31:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+var factList = [
+  "2.5 billion people need glasses but do not have them.", 
+  "624 million people are visually impaired and do not have access to eyeglasses.", 
+  "239 million schoolchildren are living with uncorrected vision.", 
+  "Spending 57 billion dollars now will give up to $228 billion benefit in the future.", 
+  "Less than 1% of global health resources were given to issues involving poor vision.", 
+  "Liberia did not have a single eye clinic until 2017."
+];
+
+var fact = document.getElementById("fact");
+var factBtn = document.getElementById("factBtn");
+var count = 0;
+
+if (factBtn) {
+  factBtn.addEventListener("click", displayFact);
+}
+
+function displayFact() {
+  fact.innerHTML = factList[count];
+  count++;
+  if (count == factList.length) {
+    count = 0;
+  }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
